44f87d9d18a40a1a0a22d30fad78ccc7d56f00e0
Harsh <67417393+denji77@users.noreply.github.com>
2025-05-13 23:38:48 +0530

    minor-5

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,4 +1,4 @@
-import { Logger } from 'jsr:@deno-library/logger';
+import { Logger } from "https://deno.land/x/logger@1.2.0/mod.ts";
 
 const logger = new Logger();
 await logger.initFileLogger('log', { rotate: true });
